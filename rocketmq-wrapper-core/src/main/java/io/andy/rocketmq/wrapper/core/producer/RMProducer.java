2aa3f10f329f4ef4070793ddabd92fa73f87e311
andy zhao <286505491@qq.com>
2020-10-29 17:05:58 +0800

    增加acl； 增加消息轨迹；

--- rocketmq-wrapper-core/src/main/java/io/andy/rocketmq/wrapper/core/producer/RMProducer.java
+++ rocketmq-wrapper-core/src/main/java/io/andy/rocketmq/wrapper/core/producer/RMProducer.java
@@ -6,16 +6,20 @@ import io.andy.rocketmq.wrapper.core.exception.MessageSendException;
 import io.andy.rocketmq.wrapper.core.producer.listener.AbstractTransactionListener;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.rocketmq.acl.common.AclClientRPCHook;
+import org.apache.rocketmq.acl.common.SessionCredentials;
 import org.apache.rocketmq.client.exception.MQBrokerException;
 import org.apache.rocketmq.client.exception.MQClientException;
-import org.apache.rocketmq.client.producer.MessageQueueSelector;
-import org.apache.rocketmq.client.producer.SendCallback;
-import org.apache.rocketmq.client.producer.SendResult;
-import org.apache.rocketmq.client.producer.TransactionMQProducer;
+import org.apache.rocketmq.client.producer.*;
 import org.apache.rocketmq.client.producer.selector.SelectMessageQueueByHash;
+import org.apache.rocketmq.client.trace.AsyncTraceDispatcher;
+import org.apache.rocketmq.client.trace.TraceDispatcher;
+import org.apache.rocketmq.client.trace.hook.SendMessageTraceHookImpl;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.common.topic.TopicValidator;
 import org.apache.rocketmq.remoting.exception.RemotingException;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
@@ -34,14 +38,20 @@ public class RMProducer  extends AbstractMQEndpoint {
     private static final int            DEFAULT_SEND_MSG_TIMEOUT = 10000;
     private int                         retryTimes = 2;
     private int                         sendMsgTimeout = DEFAULT_SEND_MSG_TIMEOUT;
+
+    private AtomicBoolean               started = new AtomicBoolean(false);
+    private boolean                     enableMsgTrace;
+
     private String                      nameSrvAddr;
     private String                      producerGroup;
     private String                      instanceName;
     private String                      unitName;
-    private AtomicBoolean               started = new AtomicBoolean(false);;
+    private String                      accessKey;
+    private String                      secretKey;
+    private String                      customizedTraceTopic = TopicValidator.RMQ_SYS_TRACE_TOPIC;
 
     private ExecutorService             checkExecutorService;
-    private TransactionMQProducer       producer;
+    private DefaultMQProducer           producer;
     private AbstractTransactionListener transactionListener;
     private MessageQueueSelector        messageQueueSelector = new SelectMessageQueueByHash();
 
@@ -55,6 +65,13 @@ public class RMProducer  extends AbstractMQEndpoint {
     public RMProducer start() {
         if (started.compareAndSet(false, true)) {
             init();
+
+            try {
+                producer.start();
+            } catch (MQClientException e) {
+                throw new RuntimeException("启动[生产者]RMProducer异常", e);
+            }
+            log.info("启动[生产者]RMProducer成功");
             return this;
         } else {
             throw new IllegalStateException("Producer: " + producerGroup + "started yet!!");
@@ -136,6 +153,51 @@ public class RMProducer  extends AbstractMQEndpoint {
         this.instanceName = instanceName;
         return this;
     }
+
+    /**
+     * @Description: 设置ACL的accessKey
+     * @date 2020-10-29
+     * @Param accessKey:
+     * @return: io.andy.rocketmq.wrapper.core.producer.RMProducer
+     */
+    public RMProducer accessKey(String accessKey) {
+        this.accessKey = accessKey;
+        return this;
+    }
+
+    /**
+     * @Description: 设置ACL的secretKey
+     * @date 2020-10-29
+     * @Param secretKey:
+     * @return: io.andy.rocketmq.wrapper.core.producer.RMProducer
+     */
+    public RMProducer secretKey(String secretKey) {
+        this.secretKey = secretKey;
+        return this;
+    }
+
+    /**
+     * @Description: 是否开启消息轨迹
+     * @date 2020-10-29
+     * @Param enableMsgTrace:
+     * @return: io.andy.rocketmq.wrapper.core.producer.RMProducer
+     */
+    public RMProducer enableMsgTrace(boolean enableMsgTrace) {
+        this.enableMsgTrace = enableMsgTrace;
+        return this;
+    }
+
+    /**
+     * @Description: 设置customizedTraceTopic
+     * @date 2020-10-29
+     * @Param customizedTraceTopic:
+     * @return: io.andy.rocketmq.wrapper.core.producer.RMProducer
+     */
+    public RMProducer customizedTraceTopic(String customizedTraceTopic) {
+        this.customizedTraceTopic = customizedTraceTopic;
+        return this;
+    }
+
 
     /***
      * @Description: 生产者发送同步/异步消息重试次数设置
@@ -527,7 +589,7 @@ public class RMProducer  extends AbstractMQEndpoint {
      * @return: org.apache.rocketmq.client.producer.SendResult
      */
     public  SendResult sendTransactional(String topic, String tags, Object req, Object arg)  throws  MQClientException{
-        if (producer.getTransactionListener() == null) {
+        if (((TransactionMQProducer)producer).getTransactionListener() == null) {
             throw new IllegalStateException("The TransactionMQProducer does not exist TransactionListener");
         }
 
@@ -548,15 +610,13 @@ public class RMProducer  extends AbstractMQEndpoint {
         Objects.requireNonNull(producerGroup);
         Objects.requireNonNull(nameSrvAddr);
 
+        createDefaultMQProducer();
 
-        transactionListener.setMessageConverter(messageConverter);
-        producer = new TransactionMQProducer(producerGroup);
         producer.setNamesrvAddr(nameSrvAddr);
-
-
         producer.setRetryTimesWhenSendFailed(retryTimes);
         producer.setRetryTimesWhenSendAsyncFailed(retryTimes);
         producer.setUnitName(unitName);
+        producer.setSendMsgTimeout(sendMsgTimeout);
 
         if (transactionListener != null) {
             initTransactionEnv();
@@ -565,15 +625,6 @@ public class RMProducer  extends AbstractMQEndpoint {
         if (StringUtils.isNotEmpty(instanceName)) {
             producer.setInstanceName(instanceName);
         }
-
-        //producer.setVipChannelEnabled(false);
-        producer.setSendMsgTimeout(sendMsgTimeout);
-        try {
-            producer.start();
-        } catch (MQClientException e) {
-            throw new RuntimeException("启动[生产者]RMProducer异常", e);
-        }
-        log.info("启动[生产者]RMProducer成功");
     }
 
     /**
@@ -583,24 +634,79 @@ public class RMProducer  extends AbstractMQEndpoint {
      * @return: void
      */
     private void initTransactionEnv() {
-        producer.setTransactionListener(transactionListener);
+        transactionListener.setMessageConverter(messageConverter);
+        ((TransactionMQProducer)producer).setTransactionListener(transactionListener);
 
         // 初始化回查线程池
         if (checkExecutorService == null) {
-            checkExecutorService = new ThreadPoolExecutor(
-                    5,
-                    512,
-                    10000L,
-                    TimeUnit.MILLISECONDS,
-                    new LinkedBlockingQueue<>(512),
-                    runnable -> {
-                        Thread thread = new Thread(runnable);
-                        thread.setName(producerGroup + "-check-thread");
-                        return null;
-                    });
+            checkExecutorService = getDefaultCheckExecutorService();
         }
 
-        producer.setExecutorService(checkExecutorService);
+        ((TransactionMQProducer)producer).setExecutorService(checkExecutorService);
+    }
+
+    /**
+     * @Description: 默认的CheckExecutorService
+     * @date 2020-10-29
+     *
+     * @return: java.util.concurrent.ExecutorService
+     */
+    private ExecutorService getDefaultCheckExecutorService() {
+        return new ThreadPoolExecutor(
+                5,
+                512,
+                10000L,
+                TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<>(512),
+                runnable -> {
+                    Thread thread = new Thread(runnable);
+                    thread.setName(producerGroup + "-check-thread");
+                    return null;
+                });
+    }
+
+    /**
+     * @Description: 检查是否配置ACL
+     * @date 2020-10-29
+     *
+     * @return: boolean
+     */
+    private boolean enabledAcl() {
+        return !StringUtils.isEmpty(accessKey) && !StringUtils.isEmpty(secretKey);
+    }
+
+
+    private void createDefaultMQProducer() {
+        boolean enabledAcl = enabledAcl();
+
+        if (enabledAcl) {
+            SessionCredentials credentials = new SessionCredentials(accessKey, secretKey);
+            producer = new TransactionMQProducer(producerGroup, new AclClientRPCHook(credentials));
+            producer.setVipChannelEnabled(false);
+        } else {
+            producer = new TransactionMQProducer(producerGroup);
+        }
+
+        enableMsgTrace(enabledAcl, customizedTraceTopic);
+    }
+
+
+    private void enableMsgTrace(boolean enabledAcl, String customizedTraceTopic) {
+        if (enableMsgTrace) {
+            try {
+                AsyncTraceDispatcher dispatcher = new AsyncTraceDispatcher(producerGroup,
+                        TraceDispatcher.Type.PRODUCE, customizedTraceTopic,
+                        enabledAcl ? new AclClientRPCHook(new SessionCredentials(accessKey, secretKey)) : null);
+                dispatcher.setHostProducer(producer.getDefaultMQProducerImpl());
+                Field field = DefaultMQProducer.class.getDeclaredField("traceDispatcher");
+                field.setAccessible(true);
+                field.set(producer, dispatcher);
+                producer.getDefaultMQProducerImpl().registerSendMessageHook(
+                        new SendMessageTraceHookImpl(dispatcher));
+            } catch (Throwable e) {
+                log.error("system trace hook init failed ,maybe can't send msg trace data");
+            }
+        }
     }
 
 }
