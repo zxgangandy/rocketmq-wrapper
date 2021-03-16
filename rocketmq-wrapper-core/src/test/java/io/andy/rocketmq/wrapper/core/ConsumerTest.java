af1672770297d12704f7ad6f5519e73a2f35c4c3
andy zhao <286505491@qq.com>
2021-03-16 14:21:54 +0800

    update version to 1.1.2

--- rocketmq-wrapper-core/src/test/java/io/andy/rocketmq/wrapper/core/ConsumerTest.java
+++ rocketmq-wrapper-core/src/test/java/io/andy/rocketmq/wrapper/core/ConsumerTest.java
@@ -2,7 +2,10 @@ package io.andy.rocketmq.wrapper.core;
 
 
 import io.andy.rocketmq.wrapper.core.consumer.RMConsumer;
+import io.andy.rocketmq.wrapper.core.consumer.processor.OrderlyProcessor;
 import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.ConsumeOrderlyStatus;
+import org.apache.rocketmq.common.message.MessageExt;
 import org.junit.Test;
 
 public class ConsumerTest {
@@ -16,6 +19,24 @@ public class ConsumerTest {
                 .concurrentlyProcessor((messageBody) -> {
                     System.out.println("concurrentlyProcessor, messageBody=" + messageBody);
                     return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+                })
+                .start();
+
+        Thread.sleep(50000);
+    }
+
+    @Test
+    public void orderlyProcessor() throws InterruptedException {
+        RMWrapper.with(RMConsumer.class)
+                .consumerGroup("consumer-test")
+                .nameSrvAddr("127.0.0.1:9876")
+                .subscribe("test")
+                .orderlyProcessor(new OrderlyProcessor<MessageExt>() {
+                    @Override
+                    public ConsumeOrderlyStatus process(MessageExt messageBody) {
+                        System.out.println("OrderlyProcessor, messageBody=" + messageBody);
+                        return ConsumeOrderlyStatus.SUCCESS;
+                    }
                 })
                 .start();
 
